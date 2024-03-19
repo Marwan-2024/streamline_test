f7c835af2407455a4a690ccf63cb12755ab69c96
Marwan-2024 <156219436+Marwan-2024@users.noreply.github.com>
2024-03-19 19:53:43 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import streamlit as st
 st. title("Hello, world!")
 # Start a new form
 with st. form(key='my_form'):
-# Create a text input field
-user_input = st.text_input (label="Enter some text")
-# Create a submit button
+  # Create a text input field
+  user_input = st.text_input (label="Enter some text")
+  # Create a submit button
 submitted = st. form_submit_button (label="Send" )
 # Display the user's input if the form was submitted
 if submitted:
-st.write(f"You entered: {user_input}")
+  st.write(f"You entered: {user_input}")
