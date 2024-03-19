92db986afbf41bee8f42dd9cff2ccc49470b5eb2
Marwan-2024 <156219436+Marwan-2024@users.noreply.github.com>
2024-03-19 19:55:18 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ with st. form(key='my_form'):
   # Create a text input field
   user_input = st.text_input (label="Enter some text")
   # Create a submit button
-submitted = st. form_submit_button (label="Send" )
+  submitted = st. form_submit_button (label="Send" )
 # Display the user's input if the form was submitted
 if submitted:
   st.write(f"You entered: {user_input}")
